24ffec0681760999173038a848262d08a6c4ae5f
Paul Mayer <paul@paul-mayer.eu>
2020-06-08 00:09:07 +0200

    init thrust test

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -5,16 +5,23 @@ var velocity: = Vector2.ZERO
 export var speed: = Vector2(300,100)
 export var gravity : = 300	
 export var thrust := Vector2(0,0)
+export var acceleration := -1.1
 
 func _physics_process(delta: float) -> void:	
-	velocity.y = gravity *delta
+	
+	var input :=  Input.get_action_strength("thrust")
+	
+	
+	velocity.y = gravity * delta 
 	velocity.y= max(velocity.y,speed.y)
+	if input ==	 1 :
+		velocity.y = velocity.y*acceleration
 	velocity = move_and_slide(velocity)
+	
 	#var direction: = Vector2(Input.get_action_strength("move_left")-Input.get_action_strength("move_right"),0)
-	#velocity= speed * direction
 	
 	var lab = get_node("Label")
-	lab.set_text(String(delta))
+	lab.set_text(String(velocity))
 	
 	return
 
