abc55f224faced51d8f13dd7670a4ff6f462fbcd
work paul <workpaul@works-Air.fritz.box>
2020-08-02 22:52:58 +0200

    test vector in physics body

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -3,7 +3,7 @@ extends Actor
 var velocityMax := Vector2(100,100)
 var force := 2
 var offset := 100
-var boosterTorque :=2
+var boosterTorque :=0
 
 func _process(delta):
 	
@@ -15,16 +15,17 @@ func _process(delta):
 	
 	if right ==	 1 :
 		get_node("fireRight").visible = true
-		var boosterForceRight = Vector2(-boosterTorque,-force)#.direction_to(-self.linear_velocity)	
+		var boosterForceRight = Vector2(0,-force)+linear_velocity#.direction_to(-self.linear_velocity)	
 		var offsetRight := Vector2(offset,0)
 		apply_impulse(offsetRight,boosterForceRight)
 		
 	if left ==	 1 :
 		get_node("fireLeft").visible = true
-		var boosterForceLeft = Vector2(boosterTorque,-force)#.direction_to(-self.linear_velocity)	
+		var boosterForceLeft = Vector2(0,-force)+linear_velocity#.direction_to(-self.linear_velocity)	
 		var offsetleft := Vector2(-offset,0)		
 		apply_impulse(offsetleft,boosterForceLeft)
 	
+	#print(linear_velocity)
 	
 	return
 
