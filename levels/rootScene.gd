72b9acc526355781c3106d9d338378c26be462d1
Paul <paul@paul-mayer.eu>
2020-10-19 21:43:53 +0200

    reset root scene

---
+++ levels/rootScene.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+#
+#func resetLevel():
+#	for i in range(0, get_child_count()):
+#			get_child(i).queue_free()
+#	var scene = load("res://levels/testLevel.tscn")
+#	var scene_instance = scene.instance()
+#	scene_instance.set_name("testLevel")
+#	add_child(scene_instance)
