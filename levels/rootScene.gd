e4f162548c5a7a8d00b74fc846d24a78aa8d4c5f
Paul <paul@paul-mayer.eu>
2020-11-06 17:34:41 +0100

    remove all fn in root

--- levels/rootScene.gd
+++ levels/rootScene.gd
@@ -1,20 +1,5 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-#
 #func resetLevel():
 #	for i in range(0, get_child_count()):
 #			get_child(i).queue_free()
